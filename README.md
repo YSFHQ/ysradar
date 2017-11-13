aa4cb6e82e01864dcefa5155b81401e2c8595961
Eric Tendian <erictendian@gmail.com>
2017-11-12 21:54:56 -0600

    Updating YSRadar server for YS 20110207 and above, adding more .point files for client

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ YSRadar server (by Vincent) and client (by WTR)
 
 ## Server
 
-From https://forum.ysfhq.com/viewtopic.php?f=236&t=1428
+From https://forum.ysfhq.com/viewtopic.php?f=236&t=1428, modified for YS network protocol 20110207 and above by Eric
 
 ## Client
 
-From https://www41.atwiki.jp/ysflight/pages/31.html
+From https://www41.atwiki.jp/ysflight/pages/31.html, with additional data files as in https://forum.ysfhq.com/viewtopic.php?f=236&t=7323
--- server/ysradar.py
+++ server/ysradar.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
+
 from socket import *
 from random import randrange
-import sys, threading, re, math,time,urllib
+import sys, os, threading, re, math, time, urllib, string, struct
 
 try: import psyco; psyco.full()
 except: pass
@@ -107,9 +109,11 @@ class SendMess30s(threading.Thread):
             self.running.wait(30)
             try:
                 self.c.send("\x04\x00\x00\x00\x11\x00\x00\x00")
-                print("30s")
+                self.c.send("\x04\x00\x00\x00\x25\x00\x00\x00")
+                #print("30s")
             except:
                 print "disconnected ?"
+                sys.exit(0)
 
 
     def stop(self):
@@ -121,20 +125,38 @@ class ThreadReception(threading.Thread):
         threading.Thread.__init__(self)
         self.connexion = conn            # ref. du socket de connexion
         self.message="0"
-
+        self.ids={}
         self.ptime=time.time()
         #self.s_c=0 #connected fully or not
     def run(self):
         s_c=0
         while 1:
-             if s_c==0:
+            if s_c==0:
                 mess = self.connexion.recv(2048)
-             else:
+            else:
                 mess = self.connexion.recv(462)
 
-             clef=""
-             if s_c==1:
-                 if time.time()-self.ptime>=refresh_rate:
+            clef=""
+            if s_c==1:
+                if mess.find('\x00\x00\x25\x00\x00\x00')!=-1:
+                    self.ids={}
+                    mess2 = mess
+                    while mess2.find('\x00\x00\x25\x00\x00\x00')!=-1:
+                        pos = mess2.find('\x00\x00\x25\x00\x00\x00') + 2
+                        packet_size = struct.unpack('I', mess2[pos-4:pos])[0]
+                        player = mess2[pos:packet_size+pos]
+                        mess2 = mess2[packet_size+pos:]
+                        try:
+                            status=struct.unpack('H', player[4:6])[0]
+                            iff=struct.unpack('H', player[6:8])[0]
+                            id=struct.unpack('I', player[8:12])[0]
+                            username=player[16:]
+                            username=username[:username.find('\x00')]
+                            if status == 1 or status==3: #flying user (1=regular, 3=server)
+                                self.ids[str(id)]='[IFF'+str(iff+1)+']'+username.replace(":","").replace(" ","")
+                        except:
+                            continue
+                if time.time()-self.ptime>=refresh_rate:
                     self.ptime=time.time()
                     if len(self.message)<5:
                         nb_t=2
@@ -146,14 +168,14 @@ class ThreadReception(threading.Thread):
                     if s_c==1:
                         pos_mess=-1
                         c=0
-                        while mess[0:5]!="\x49\x00\x00\x00\x0b" and c<nb_t:
+
+                        while mess[0:5]!="\x49\x00\x00\x00\x0B" and c<nb_t:
                             #print "not found" + str(hex(ord(mess[0])))
                             mess=self.connexion.recv(462)
                             if len(mess)!=462:# and mess[0]!="\x00":
                                 c+=1
 
-
-                        if mess[0:5]=="\x49\x00\x00\x00\x0b":
+                        if mess[0:5]=="\x49\x00\x00\x00\x0B":
                             pos_mess=0
                         while pos_mess!=-1:
                             pos_z=ieee_to_int(cut_coord(mess[22+pos_mess:26+pos_mess]))/0.3048
@@ -162,7 +184,9 @@ class ThreadReception(threading.Thread):
                                 if self.message=="0":
                                     self.message=""
                                 id2=hex(ord(mess[12+pos_mess:13+pos_mess]))[2:]
-                                id=int("100"+id2,16)
+                                id3=hex(ord(mess[13+pos_mess:14+pos_mess])+256)[2:]
+                                id=int(str(id3)+id2,16)
+                                #print id
                                 pos_x=ieee_to_int(cut_coord(mess[18+pos_mess:22+pos_mess]))
                                 pos_y=ieee_to_int(cut_coord(mess[26+pos_mess:30+pos_mess]))
                                 try:
@@ -174,8 +198,14 @@ class ThreadReception(threading.Thread):
 
                                 heading=int(cut_coord(mess[30+pos_mess:32+pos_mess]),16)*360/65535.0
                                 self.ptime=time.time()
-
-                                self.message+=str(id)+":"+str(int(pos_x)*0.053996)+":"+str(int(pos_z/10))+":"+str(int(pos_y)*0.053996)+":"+str(int(heading))+":"+str(int(speed))+"\\"
+                                name = ""
+                                #for k, v in self.ids.items():
+                                    #print k+' '+str(id)
+                                if str(id) in self.ids:
+                                    name=self.ids[str(id)]
+                                else:
+                                    name=str(id)
+                                self.message+=name+":"+str(int(pos_x)*0.053996)+":"+str(int(pos_z/10))+":"+str(int(pos_y)*0.053996)+":"+str(int(heading))+":"+str(int(speed))+"\\"
                                 #print "T>> ",self.message
                             pos_mess=mess.find("\x49\x00\x00\x00\x0b",pos_mess+1)
                             #print "pu",pos_mess
@@ -191,43 +221,45 @@ class ThreadReception(threading.Thread):
                             print str(cle)+" left"
                         #print self.message
                         #print dic_online[key],pos_x,pos_y,pos_z
-                 if clef!="":
+                if clef!="":
                     conn_client.__delitem__(clef)
 
-             if s_c==0:
-                 print "do it"
-                 if mess.find("\x04\x00\x00\x00\x10\x00\x00\x00")!=-1:
+            if s_c==0:
+                print "do it"
+                if mess.find("\x04\x00\x00\x00\x10\x00\x00\x00")!=-1:
                     print "end login"
                     s_c=1
-                    self.connexion.send("\x0c\x00\x00\x00\x06\x00\x00\x00\x07\x00\x00\x00\x00\x00\x00\x00")
+                    self.connexion.send("\x0C\x00\x00\x00\x06\x00\x00\x00\x07\x00\x00\x00\x00\x00\x00\x00")
                     self.connexion.send("\x08\x00\x00\x00\x26\x00\x00\x00\x00\x00\x00\x00")
+                    self.connexion.send("\x04\x00\x00\x00\x25\x00\x00\x00")
                     th_server = Server()
                     th_server.start()
 
-                 if mess.find("\x00\x00\x2c\x00\x00\x00\x01")!=-1:
-                    adebug(mess)
+                if mess.find("\x00\x00\x2C\x00\x00\x00\x01")!=-1:
+                    #adebug(mess)
                     print "sending airplane list"
                     self.connexion.send(mess)
 
-             if mess.find("\x1c\x00\x00\x00\x21\x00\x00\x00\x01\x00")!=-1 or mess.find("\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x40\x9c\x46")!=-1:
+            if mess.find("\x1C\x00\x00\x00\x21\x00\x00\x00\x01\x00")!=-1 or mess.find("\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x40\x9C\x46")!=-1:
                 print "almost"
-                self.connexion.send("\x0c\x00\x00\x00\x06\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00")
+                self.connexion.send("\x0C\x00\x00\x00\x06\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00")
                 self.connexion.send("\x08\x00\x00\x00\x26\x00\x00\x00\x00\x00\x00\x00")
 
-             if mess =='':
-                 break
-             if mess.find('\x00\x00\x00\x20\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')!=-1:
-                 #print "mess"
-                 print mess[mess.find('\x00\x00\x00\x20\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')+15:]
-
-
+            if mess =='':
+                break
+            if mess.find('\x00\x00\x00\x20\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')!=-1:
+                #print "mess"
+                chat = mess[mess.find('\x00\x00\x00\x20\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')+15:]
+                #chat = chat[:chat.find('\x00')]
+                if chat.find('took off (')!=-1 or chat.find('has left the airplane')!=-1:
+                    self.connexion.send("\x04\x00\x00\x00\x25\x00\x00\x00")
 
         # On force la fermeture du thread <emission> :
-        th_E._Thread__stop()
+        #th_E._Thread__stop()
         print "***Connection stopped***"
         self.connexion.close()
-        #st=raw_input("Press <ENTER> to exit")
-        sys.exit()
+        st=raw_input("Press <ENTER> to exit")
+        os._exit(1)
 
 class ThreadServerList(threading.Thread):
     """thread which send the messages"""
@@ -262,15 +294,20 @@ class ThreadServerList(threading.Thread):
 ##if p.read().find("0")!=-1:
 ##    print "You cannot use this version now, update YSChat to use the radar for the YSATC radar"
 ##    st=raw_input("Press <ENTER> to exit...")
-##    sys.exit()
+##    sys.exit(0)
 s_c=0
 c=socket()
 
 port=7915
+#alt_limit=10.0
+#refresh_rate=3
+#sensibility=3
+conversion=20110207
 alt_limit=input("Altitude limit? (enter a number in feet, efficient if upper than 100) ")/10.0 #in feet
 refresh_rate=input("Refresh rate in seconds? (1.25 -> 5) ")
 sensibility=input("Sensibility? (choose 3 for a map without motion path, else choose 6) ")
-conversion=input("YSFS version? ")
+ipaddr=raw_input("IP Address? ")
+#conversion=input("YSFS version? ")
 conn_version=hex(conversion)
 conn_version=chr(int(conn_version[len(conn_version)-2:len(conn_version)],16))+chr(int(conn_version[len(conn_version)-4:len(conn_version)-2],16))+chr(int(conn_version[len(conn_version)-6:len(conn_version)-4],16))+chr(int(conn_version[2:len(conn_version)-6]))
 mySocket = socket(AF_INET,SOCK_STREAM)
@@ -290,7 +327,7 @@ class Server(threading.Thread):
         except:
             print "The server failed to start"
             st=raw_input("Press <ENTER> to exit...")
-            sys.exit()
+            sys.exit(0)
         print "Server ready, waiting for requests..."
         mySocket.listen(30)
 
@@ -306,16 +343,16 @@ class Server(threading.Thread):
             (it, adresse[0], adresse[1])
             connexion.send("0\r\n")
 
-
+ids={}
 ips=""
 ip_info=getaddrinfo(gethostname(), None)
 for nb in ip_info:
     ips+="|"+nb[4][0]
 try:
-    c.connect(('127.0.0.1',port))
+    c.connect((ipaddr,port))
 except:
     print "failed to connect"
-    sys.exit()
+    sys.exit(0)
 #username=raw_input('USERNAME (15 characters max) ? ')
 players_x={}
 players_y={}
@@ -325,27 +362,35 @@ username='\x18\x00\x00\x00\x01\x00\x00\x00'+username
 username+='\x00'+conn_version
 c.send(username)
 mess1=c.recv(1024)
-if mess1[:5]=="\x08\x00\x00\x00\x1d":
-    print "version "+ mess1[8:12]
+#if mess1[:5]=="\x08\x00\x00\x00\x1D":
+    #print "version "+ mess1[8:12]
 if len(mess1)<12:
     mess1=c.recv(1024)
 mt="\x00"*28
-adebug(mess1)
+#adebug(mess1)
 if mess1.find("RADARALTI")!=-1:
     pos1=mess1.find("RADARALTI")-12
     print "found"
 else:
     print "not found"
     pos1=mess1.find("\x40",20)
-ms="\x0c\x00\x00\x00\x06\x00\x00\x00\x09\x00\x00\x00\x00\x00\x00\x00\x0c\x00\x00\x00\x06\x00\x00\x00\x0a\x00\x00\x00\x00\x00\x00\x00\x0c\x00\x00\x00\x06\x00\x00\x00\x0b\x00\x00\x00\x00\x00\x00\x00"+mess1[pos1:mess1.find(mt)+28]+"\x00\x04\x00\x00\x00\x21\x00\x00"+mess1[mess1.find(mt)+28:]
-adebug(ms[:48])
+#ms="\x0C\x00\x00\x00\x06\x00\x00\x00\x09\x00\x00\x00\x00\x00\x00\x00\x0C\x00\x00\x00\x06\x00\x00\x00\x0A\x00\x00\x00\x00\x00\x00\x00\x0C\x00\x00\x00\x06\x00\x00\x00\x0B\x00\x00\x00\x00\x00\x00\x00"+mess1[pos1:mess1.find(mt)+28]+"\x00\x04\x00\x00\x00\x21\x00\x00"+mess1[mess1.find(mt)+28:]
+ms="\x0C\x00\x00\x00\x06\x00\x00\x00\x09\x00\x00\x00\x00\x00\x00\x00\x0C\x00\x00\x00\x06\x00\x00\x00\x0A\x00\x00\x00\x00\x00\x00\x00\x0C\x00\x00\x00\x06\x00\x00\x00\x0B\x00\x00\x00\x00\x00\x00\x00"+mess1[pos1:mess1.find(mt)+28].replace("\x00\x40\x00\x00\x00\x04\x00\x00\x00","\x00\x04\x00\x00\x00\x21\x00\x00\x00\x40\x00\x00\x00\x04\x00\x00\x00")+mess1[mess1.find(mt)+28:]
+#adebug(ms[:48])
 #raw_input("press enter")
 c.send(ms)
 dic_online={}
 
-th_E      = ThreadServerList()
-th_R      = ThreadReception(c)
-thread30s = SendMess30s(c)
-th_R.start()
-th_E.start()
-thread30s.start()
+try:
+    #th_E      = ThreadServerList()
+    #th_E.daemon = True
+    th_R      = ThreadReception(c)
+    th_R.daemon = True
+    thread30s = SendMess30s(c)
+    thread30s.daemon = True
+    th_R.start()
+    #th_E.start()
+    thread30s.start()
+    while True: time.sleep(100)
+except (KeyboardInterrupt, SystemExit):
+    print '\n! Received keyboard interrupt, quitting threads.\n'
